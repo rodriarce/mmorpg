9d0b10736bff42785bf1fe32e72b216a52a10f58
rodriarce <rodrigoarce91@gmail.com>
2020-02-21 17:01:38 -0300

    new Game

---
+++ MMORPG GAME/Assets/Scripts/Player/AtackPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackPlayer : MonoBehaviour
+{
+
+    private Animator animPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+          animPlayer=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+
+
+
+
+    }
+}
---
+++ MMORPG GAME/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controllerPy;
+    private Vector3 movePlayer=Vector3.zero;
+    public float speed=6f;
+    public float gravity=20f;
+    private Animator animPlayer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controllerPy=GetComponent<CharacterController>();
+        animPlayer=GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+
+      private void MovePlayer()
+      {
+
+          if(controllerPy.isGrounded)
+        {
+            movePlayer=new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+            movePlayer=transform.TransformDirection(movePlayer);
+            movePlayer*=speed;
+
+
+
+
+
+
+        }
+        movePlayer.y-=gravity*Time.deltaTime;
+        controllerPy.Move(movePlayer*Time.deltaTime);
+
+          animPlayer.SetBool("isWalking",true);
+
+          if(controllerPy.velocity==Vector3.zero)
+          {
+              animPlayer.SetBool("isWalking",false);
+          }
+
+
+
+
+
+
+      }
+
+
+
+
+
+
+
+}
